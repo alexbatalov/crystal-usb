91ec7e80a50c524ade21888534e49286844721b3
Alexander Batalov <alex.batalov@gmail.com>
2018-12-06 23:01:51 +0300

    Add convenience getters for configuration attributes

--- src/usb/configuration.cr
+++ src/usb/configuration.cr
@@ -1,4 +1,7 @@
 struct USB::Configuration
+  private SELF_POWERED_MASK  = 1 << 6
+  private REMOTE_WAKEUP_MASK = 1 << 5
+
   getter configuration_value : UInt8
   getter attributes : UInt8
   getter max_power : UInt8
@@ -6,5 +9,13 @@ struct USB::Configuration
   getter extra : Bytes?
 
   def initialize(@configuration_value : UInt8, @attributes : UInt8, @max_power : UInt8, @interfaces : Array(Interface), @extra : Bytes?)
+  end
+
+  def self_powered?
+    attributes.bits_set?(SELF_POWERED_MASK)
+  end
+
+  def remote_wakeup?
+    attributes.bits_set?(REMOTE_WAKEUP_MASK)
   end
 end
