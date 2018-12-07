6965f56b7ec27ecc9e3702c2d1367c5066f64272
Alexander Batalov <alex.batalov@gmail.com>
2018-12-07 16:18:49 +0300

    Add convenience getters for endpoint attributes

--- src/usb/endpoint.cr
+++ src/usb/endpoint.cr
@@ -7,4 +7,24 @@ struct USB::Endpoint
 
   def initialize(@endpoint_address : UInt8, @attributes : UInt8, @interval : UInt8, @max_packet_size : UInt16, @extra : Bytes?)
   end
+
+  def endpoint_number
+    @endpoint_address & LibUSB::ENDPOINT_NUMBER_MASK
+  end
+
+  def direction
+    LibUSB::EndpointDirection.new(@endpoint_address & LibUSB::ENDPOINT_DIRECTION_MASK)
+  end
+
+  def transfer_type
+    LibUSB::TransferType.new(@attributes & LibUSB::ENDPOINT_TRANSFER_TYPE_MASK)
+  end
+
+  def iso_sync_type
+    LibUSB::IsoSyncType.new((@attributes & LibUSB::ENDPOINT_ISO_SYNC_TYPE_MASK) >> 2) if transfer_type.isochronous?
+  end
+
+  def iso_usage_type
+    LibUSB::IsoUsageType.new((@attributes & LibUSB::ENDPOINT_ISO_USAGE_TYPE_MASK) >> 4) if transfer_type.isochronous?
+  end
 end
